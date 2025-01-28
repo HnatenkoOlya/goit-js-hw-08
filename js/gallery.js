15d5bc87e8024e05e0476b8ceac20a03c06e2ebf
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2025-01-28 19:55:49 +0100

    hw8.2

--- js/gallery.js
+++ js/gallery.js
@@ -82,12 +82,21 @@ const images = [
     //const link = document.querySelector(".gallery-link");
     //link = link.preventDefault();
 
-    gallery.addEventListener("click", event => {
+    gallery.addEventListener("click", clickGallery);
+    function clickGallery(event)  {
         event.preventDefault();
-          //console.log(images.original)
           const link = event.target.closest(".gallery-image");
-          if (link) {
-            const largeImage = link.dataset.source;
-            console.log(largeImage); 
+          if (!link) {
+            //const largeImage = link.dataset.source;
+            //console.log(largeImage); 
+            return;
           }
-    })
+          const largeImage = link.dataset.source;
+          const instance = basicLightbox.create(`
+            <img src="${largeImage}" width="800" height="600">
+        `)
+        
+        instance.show()
+    }
+    
+   
